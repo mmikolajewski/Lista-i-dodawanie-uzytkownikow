450b7babcf58831951e45d2fd2b3ad8291492bf6
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2023-04-01 10:59:27 +0100

    Starter

---
+++ src/main/java/com/ciastek/list/ListaIDodawanieUzytkownikowApplication.java
@@ -0,0 +1,13 @@
+package com.ciastek.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaIDodawanieUzytkownikowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListaIDodawanieUzytkownikowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ciastek/list/ListaIDodawanieUzytkownikowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciastek.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaIDodawanieUzytkownikowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
