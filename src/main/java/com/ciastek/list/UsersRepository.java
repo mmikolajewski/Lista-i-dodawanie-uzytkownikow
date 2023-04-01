6b97e1ef54f181209984a995a5502786f7fbe76f
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2023-04-01 14:22:01 +0100

    Rowzwiązanie

---
+++ src/main/java/com/ciastek/list/UsersRepository.java
@@ -0,0 +1,25 @@
+package com.ciastek.list;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsersRepository {
+    List<User> users = new ArrayList<>();
+
+    public UsersRepository() {
+        users.add(new User("Jan", "Wiedział", 21));
+        users.add(new User("Paweł", "Słyszał", 37));
+        users.add(new User("Karol", "Zapomniał", 17));
+    }
+
+    public List<User> getAll() {
+        return users;
+    }
+
+    public void add(User user) {
+        users.add(user);
+    }
+}
